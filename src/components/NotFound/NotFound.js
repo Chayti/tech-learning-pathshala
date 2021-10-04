2ef122e708e9b4250cb80e545ff465537e54e2a7
Chayti Saha <chaytisaha98@gmail.com>
2021-10-04 17:04:11 +0600

    not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router";
 import './NotFound.css';
 
 const NotFound = () =>{
+
+    const history = useHistory();
+
+    const handleClick = () =>{
+        history.push("/home");
+    }
+
     return (
-        <div>
-            <h2>notFound</h2>
+        <div className="container">
+            <div className="not-found-bg"></div>
+            <div className="text-center">
+                <h2 className="fw-bold">Oops!!! The page you are requesting is not found!</h2>
+                <Button onClick={handleClick} variant="outline-warning">Back to HOME</Button>
+            </div>
         </div>
     );
 }
